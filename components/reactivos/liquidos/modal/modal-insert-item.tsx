fe4162721106ae1506422f8163879fea64bdd618
ValeriaJahzeel <vcastanonh2100@alumno.ipn.mx>
2024-04-26 21:23:49 -0600

    formulario intersccion 2.0

--- components/reactivos/liquidos/modal/modal-insert-item.tsx
+++ components/reactivos/liquidos/modal/modal-insert-item.tsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import {
   Modal,
   Typography,
-  Paper,
   Stack,
   TextField,
   Button,
-} from "@mui/material";
+  IconButton,
+  Sheet,
+  FormLabel,
+  Input,
+} from "@mui/joy";
 
 import { insertRL } from "../fetching/insertRL";
+import { ModalClose } from "@mui/joy";
 
 interface ReactivosComponentProps {
   onClose: () => void;
@@ -45,106 +49,130 @@ const ExampleComponent = ({ onClose, onInsert }: ReactivosComponentProps) => {
     <React.Fragment>
       <Modal
         open={true}
+        onClose={onClose}
         aria-labelledby="modal-title"
         aria-describedby="modal-desc"
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <Paper
-          sx={{ maxWidth: 500, borderRadius: "md", p: 3, boxShadow: "lg" }}
+        <Sheet
+          variant="outlined"
+          sx={{
+            maxWidth: 500,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+          }}
         >
-          <Typography component="h2" id="modal-title" variant="h4" mb={1}>
+          <ModalClose variant="plain" sx={{ m: 1 }} onClick={onClose} />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            ontWeight="lg"
+            mb={1}
+          >
             Insertar reactivo
           </Typography>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <Stack spacing={2}>
-              <TextField
-                id="num_css"
-                name="num_cas"
-                label="Número CAS"
-                value={formData.num_cas}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="nombre"
-                name="nombre"
-                label="Nombre"
-                value={formData.nombre}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="formula"
-                name="formula"
-                label="Fórmula"
-                value={formData.formula}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="contenedor"
-                name="contenedor"
-                label="Contenedor"
-                value={formData.contenedor}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="marca"
-                name="marca"
-                label="Marca"
-                value={formData.marca}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="cantidad"
-                name="cantidad"
-                label="Cantidad (frascos)"
-                value={formData.cantidad}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="ubicacion"
-                name="ubicacion"
-                label="Ubicación"
-                value={formData.ubicacion}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-              />
-              <TextField
-                id="observaciones"
-                name="observaciones"
-                label="Observaciones"
-                value={formData.observaciones}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-                multiline
-                rows={4}
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Insertar reactivo
-              </Button>
-            </Stack>
-          </form>
-        </Paper>
+
+          <Typography id="modal-desc" textColor="text.tertiary">
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={2}>
+                <FormLabel>
+                  Número CAS:{" "}
+                  <Input
+                    id="num_css"
+                    name="num_cas"
+                    value={formData.num_cas}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+
+                <FormLabel htmlFor="nombre">
+                  Nombre:{" "}
+                  <Input
+                    id="nombre"
+                    name="nombre"
+                    value={formData.nombre}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+
+                <FormLabel>
+                  Fórmula:{" "}
+                  <Input
+                    id="formula"
+                    name="formula"
+                    value={formData.formula}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+                <FormLabel>
+                  Contenedor:{" "}
+                  <Input
+                    id="contenedor"
+                    name="contenedor"
+                    value={formData.contenedor}
+                    onChange={handleChange}
+                    className="ml-3 mt-2"
+                  />{" "}
+                </FormLabel>
+                <FormLabel htmlFor="marca">
+                  Marca:{" "}
+                  <Input
+                    id="marca"
+                    name="marca"
+                    value={formData.marca}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+                <FormLabel>
+                  Cantidad (frascos):{" "}
+                  <Input
+                    id="cantidad"
+                    name="cantidad"
+                    value={formData.cantidad}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+                <FormLabel>
+                  Ubicación:{" "}
+                  <Input
+                    id="ubicacion"
+                    name="ubicacion"
+                    value={formData.ubicacion}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+                <FormLabel>
+                  Observaciones:{" "}
+                  <Input
+                    id="observaciones"
+                    name="observaciones"
+                    value={formData.observaciones}
+                    className="ml-3 mt-2"
+                    onChange={handleChange}
+                  />{" "}
+                </FormLabel>
+                <Button type="submit" fullWidth>
+                  Insertar reactivo
+                </Button>
+              </Stack>
+            </form>
+          </Typography>
+        </Sheet>
       </Modal>
     </React.Fragment>
   );
 };
-
 export default ExampleComponent;
