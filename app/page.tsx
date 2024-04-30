2866c6477ecb701c1086105fe52ec9924f1827af
MikeszIPN <msanchezz2100@alumno.ipn.mx>
2024-04-30 12:39:03 -0600

    Orion page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <main>
       <div className="">
-        <Spline scene="https://prod.spline.design/QYlmUsZRji0nGU2N/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/QYlmUsZRji0nGU2N/scene.splinecode" />
       </div>
     </main>
   );
